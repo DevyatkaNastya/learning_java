f1bbe6fcc44ed0b2a87ba1a5debe77355fc877b3
Nastya_dev <devyatkanastya@gmail.com>
2019-07-07 21:12:42 +0300

    add isNan and isInfinite

--- DoubleDemo/src/DoubleDemo.java
+++ DoubleDemo/src/DoubleDemo.java
@@ -4,5 +4,14 @@ public class DoubleDemo {
         Double d2 = new Double("314159E-5");
 
         System.out.println(d1 + " = " + d2 + " -> " + d1.equals(d2));
+
+        Double d3 = new Double(1/0.);
+        Double d4 = new Double(0/0.);
+
+        System.out.println(d3 + ": " +d3.isInfinite() + ", " +
+                d3.isNaN());
+
+        System.out.println(d4 + ": " + d4.isInfinite() + ", " +
+                d4.isNaN());
     }
 }
