a674deffded78eeb55f279c3f3e010c3cc2fd35f
Nastya_dev <devyatkanastya@gmail.com>
2019-07-13 01:38:59 +0300

    Math.toRadians, Math.toDegrees

---
+++ Angles/src/Angles.java
@@ -0,0 +1,11 @@
+public class Angles {
+    public static void main(String args[]) {
+        double theta = 120.0;
+
+        System.out.println(theta + " angle = " + Math.toRadians(theta) + " rad");
+
+        theta = 1.312;
+
+        System.out.println(theta + " rad = " + Math.toDegrees(theta) + " angle");
+    }
+}
