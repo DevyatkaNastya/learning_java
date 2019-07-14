0e8e3e78ed1012ff033ea5b9442aa07c26bfdd0c
Nastya_dev <devyatkanastya@gmail.com>
2019-07-14 16:06:57 +0300

    ThreadGroup class

---
+++ ThreadGroupDemo/src/NewThread.java
@@ -0,0 +1,36 @@
+public class NewThread extends Thread {
+    boolean suspendFlag;
+
+    NewThread(String threadname, ThreadGroup tgOb) {
+        super(tgOb, threadname);
+        System.out.println("New thread:" + this);
+        suspendFlag = false;
+        start();
+    }
+
+    public void run() {
+        try {
+            for(int i = 5; i > 0; i--){
+                System.out.println(getName() + ": " + i);
+                Thread.sleep(1000);
+                synchronized (this){
+                while (suspendFlag){
+                    wait();
+                }
+            }
+        }
+    } catch (Exception e) {
+        System.out.println("Exception in " + getName());
+    }
+    System.out.println(getName() + " terminated.");
+}
+
+synchronized void mysuspend(){
+    suspendFlag = true;
+}
+
+synchronized void myresume(){
+    suspendFlag = false;
+    notify();
+}
+}
