801efa67cf250f064f897063de3ba744c2107f3b
Nastya_dev <devyatkanastya@gmail.com>
2019-07-27 22:20:28 +0300

    Stack by Array deque

---
+++ ArrayDequeDemo/src/ArrayDequeDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ArrayDequeDemo {
+    public static void main(String args[]){
+        ArrayDeque<String> adq = new ArrayDeque<>();
+
+        adq.push("A");
+        adq.push("B");
+        adq.push("D");
+        adq.push("E");
+        adq.push("F");
+
+        while(adq.peek() != null)
+            System.out.print(adq.pop() + " ");
+
+        System.out.println();
+    }
+}
